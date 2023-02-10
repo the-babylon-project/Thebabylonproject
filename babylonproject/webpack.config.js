5a944093a9335cf77cd6079bd70830b58a898bd7
Dylan Miller <103227705+jibbajab@users.noreply.github.com>
2023-02-09 22:42:57 -0500

    working on physics

---
+++ babylonproject/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    node: {
+        fs: 'empty'
+    },
+    resolve: {
+        fallback: {
+            path: { "path": require.resolve("path-browserify") },
+            stream: {"stream": require.resolve("stream-browserify")},
+            fs: false
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    }
+};
