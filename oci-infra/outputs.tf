484d697cf47e4cbc3dc43e7d791e89fb69be885e
luntan-maker <lucas.orman@gmail.com>
2023-02-12 13:42:45 -0800

    Added an object storage

--- oci-infra/outputs.tf
+++ oci-infra/outputs.tf
@@ -1,3 +1,10 @@
 output "k8s-cluster-id" {
   value = oci_containerengine_cluster.k8s_cluster.id
 }
+
+
+
+# For the bucket in dev
+output "buckets" {
+  value = oci_objectstorage_bucket.dev_bucket.name
+}
--- oci-infra/variables.tf
+++ oci-infra/variables.tf
@@ -12,3 +12,28 @@ variable "ssh_public_key" {
   type        = string
   description = "The SSH public key to use for connecting to the worker nodes"
 }
+
+# variable "tenancy_ocid" {
+#   default = ""
+# }
+
+# variable "user_ocid" {
+#   default = ""
+# }
+
+# variable "fingerprint" {
+#   default = ""
+# }
+
+# variable "private_key_path" {
+#   default = ""
+# }
+
+# variable "compartment_ocid" {
+#   default = ""
+# }
+
+# variable "kms_key_ocid" {
+#   default = ""
+# }
+
